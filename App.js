195acfd09442ac24b3dad10cb3917c78d40746a2
VedanshGupta <68778187+VedanshGupta@users.noreply.github.com>
2021-01-14 12:38:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import dictionary from '../localdb';
 
-export default function App() {
-  return (
-    <View>
-      <HomeScreen />
-    </View>
-  )
+export default class App extends Component{
+	render(){
+	  return (
+	    <View>
+	      <HomeScreen />
+	    </View>
+	  )
+}
 }
